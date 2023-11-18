c2ce7b69f609727a248c8b983a69c813851fe95a
Lakan Santos <rizesuoracle25@gmail.com>
2023-11-18 23:38:17 +0800

    Added error and loading handling boilerplate

---
+++ app/error.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      Something went wrong! Please try again{" "}
+      <button
+        className="bg-red-500 p-2 my-3 text-white"
+        onClick={() => window.location.replace("/")}
+      >
+        Refresh!
+      </button>
+    </div>
+  );
+};
+
+export default Error;
