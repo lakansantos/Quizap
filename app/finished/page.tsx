b5e524956b4f43ea946feb554363fd6e08dfebdc
Lakan Santos <rizesuoracle25@gmail.com>
2023-11-22 21:12:02 +0800

    Fix finish

---
+++ app/finished/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import Finish from "../modules/Finish";
+
+const page = ({searchParams}: {searchParams: {score: string}}) => {
+  const {score} = searchParams;
+  return (
+    <div className="h-[100vh]">
+      <Finish score={Number(score)} />
+    </div>
+  );
+};
+
+export default page;
