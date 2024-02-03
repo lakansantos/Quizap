04c7fea75e5407b7e64fc55a38070276fbe3765c
Lakan Santos <rizesuoracle25@gmail.com>
2024-02-03 11:50:42 +0800

    Removed useGet Function in fetches

--- app/fetches/fetches.ts
+++ app/fetches/fetches.ts
@@ -1,11 +1,5 @@
 import {API_URL_QUESTIONS} from "../constants/environment";
-import {fetcher, useGetFetch} from "../utils/fetcher";
-
-export const useGetQuestions = () => {
-  const key = ["questions"];
-  const url = "https://the-trivia-api.com/v2/questions";
-  return useGetFetch(key, url);
-};
+import {fetcher} from "../utils/fetcher";
 
 export const getQuestion = (params: object): Promise<QuestionsData[]> =>
   fetcher("GET", API_URL_QUESTIONS, params);
